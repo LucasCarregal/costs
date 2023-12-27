7012630204c747aa0c2b450ce922cc6dea6b4da9
Lucas Carregal <lucascarregal123@gmail.com>
2023-12-27 13:41:46 -0300

    Finalização layout projeto

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 import Container from "./components/layout/Container";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/newproject" element={<NewProject />} />
-      </Routes>
-      <p>Footer</p>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/Projects" element={<Projects />} />
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   );
 }
